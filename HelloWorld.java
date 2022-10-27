0d9859a2188b97ace05160c2482868f73ab50acb
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-10-27 16:29:56 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
-our program begins with a call to main(). 
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
     } 
 }
-
